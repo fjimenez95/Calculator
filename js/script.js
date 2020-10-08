c608b9fbda27c41caf1267b20fabd4860ea5861e
Nicholas Taveras <nicholastaveras@Nicholass-iMac.local>
2020-10-07 20:12:50 -0400

    First Draft of Calculator

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/*----- constants -----*/
+// Model
+
+/*----- app's state (variables) -----*/
+let calculator;
+let userInput = '';
+/*----- cached element references -----*/
+const $calculatorEl = $('#calculator');
+const $calcBtnEls = $('.calcBtn');
+const $input = $('#input');
+const $resultEl = $('#result');
+const $clearEl = $('#reset');
+/*----- event listeners -----*/
+$calcBtnEls.on('click', handleInput);
+$resultEl.on('click', handleResult);
+$clearEl.on('click', render);
+/*----- functions -----*/
+function handleInput(event) {
+    // Cache the inner text of the btn
+    // Push inner text into input
+    userInput += event.target.dataset.index;
+    $($input).attr('value', userInput);
+}
+
+function handleResult() {
+    $($input).attr('value', eval(userInput));
+}
+
+function render() {
+    $($input).attr('value', '');
+}
